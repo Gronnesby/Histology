c38f5fece2b77e90acbe46398aa5dcbc6534c138
Gronnesby <morten.gronnesby@uit.no>
2017-02-04 14:15:06 +0100

    Added a freedraw option to the leaflet map, which seems to be more useful than polygons. Should add some controls to toggle freedraw as well.

--- run.sh
+++
@@ -1,2 +0,0 @@
-./createtiles.sh static/images/pathology/slice.jpg static/images/tiles
-python app.py
--- static/js/map.js
+++ static/js/map.js
@@ -1,76 +1,24 @@
 
-var img = [13466, 8467]
-var map = L.map('map', {drawControl: true, minZoom: 0})
-var rc = new L.RasterCoords(map, img)
+var img = [13466, 8467];
+var map = L.map('map', {minZoom: 0, zoomControl: false, zoomSnap: 0});
+var rc = new L.RasterCoords(map, img);
+
 // set max zoom Level (might be `x` if gdal2tiles was called with `-z 0-x` option)
-map.setMaxZoom(rc.zoomLevel())
+map.setMaxZoom(6);
+map.options.minZoom = 2;
+
 // all coordinates need to be unprojected using the `unproject` method
 // set the view in the lower right edge of the image
-map.setView(rc.unproject([img[0] / 2, img[1] / 2]), 3)
+map.setView(rc.unproject([img[0] / 2, img[1] / 2]), 3);
 
 // the tile layer containing the image generated with `gdal2tiles --leaflet -p raster -w none <img> tiles`
 L.tileLayer('./static/images/tiles/{z}/{x}/{y}.png', {
   noWrap: true
-}).addTo(map)
+}).addTo(map);
 
-var MyCustomMarker = L.Icon.extend({
-    options: {
-        shadowUrl: null,
-        iconAnchor: new L.Point(12, 12),
-        iconSize: new L.Point(24, 24),
-        iconUrl: './static/css/images/marker-icon.png'
-    }
+var freedraw = new FreeDraw({
+    mode: FreeDraw.CREATE | FreeDraw.DELETE
 });
+map.addLayer(freedraw);
 
 
-var editableLayers = new L.FeatureGroup();
-map.addLayer(editableLayers);
-
-var options = {
-    position: 'topright',
-    draw: {
-        polyline: {
-            shapeOptions: {
-                color: '#2C7CFF',
-                weight: 10
-            }
-        },
-        polygon: {
-            allowIntersection: false, // Restricts shapes to simple polygons
-            drawError: {
-                color: '#e1e100', // Color the shape will turn when intersects
-                message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
-            },
-            shapeOptions: {
-                color: '#2C7CFF'
-            }
-        },
-        circle: false, // Turns off this drawing tool
-        rectangle: {
-            shapeOptions: {
-                clickable: false
-            }
-        },
-        marker: {
-            icon: new MyCustomMarker()
-        }
-    },
-    edit: {
-        featureGroup: editableLayers, //REQUIRED!!
-        remove: false
-    }
-};
-
-var drawControl = new L.Control.Draw(options);
-map.addControl(drawControl);
-
-map.on(L.Draw.Event.CREATED, function (e) {
-    var type = e.layerType,
-        layer = e.layer;
-
-    if (type === 'marker') {
-        layer.bindPopup('');
-    }
-
-    editableLayers.addLayer(layer);
-});
